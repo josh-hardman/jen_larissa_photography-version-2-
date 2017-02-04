392ef2ffe05f84b7c5b89ac236912395b0203637
Joshua Hardman <jhardman0830@gmail.com>
2017-02-03 20:07:45 -0700

    Messing around with animation timings

--- public/js/landingPage.js
+++ public/js/landingPage.js
@@ -1,8 +1,14 @@
-window.setTimeout(function(){
-  $('.js-background').addClass('hidden');
-  $('.item').addClass('moveRight');
-},2000);
+$(document).ready(function(){
+  window.setTimeout(function(){
+    $('.name-plaque').addClass('visible');
+  },700);
 
-window.setTimeout(function(){
-  $('.button-container').addClass('visible');
-},4500);
+  window.setTimeout(function(){
+    $('.js-background').addClass('hidden');
+    $('.item').addClass('moveRight');
+  },3000);
+
+  window.setTimeout(function(){
+    $('.button-container').addClass('visible');
+  },5500);
+});
