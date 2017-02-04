1561142c5bb17a1b406459a308228300a0765ceb
Joshua Hardman <jhardman0830@gmail.com>
2017-02-03 19:57:46 -0700

    Adding animations and dquick delay to create lazy load effect

---
+++ public/js/landingPage.js
@@ -0,0 +1,8 @@
+window.setTimeout(function(){
+  $('.js-background').addClass('hidden');
+  $('.item').addClass('moveRight');
+},2000);
+
+window.setTimeout(function(){
+  $('.button-container').addClass('visible');
+},4500);
