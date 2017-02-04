9ea52e1ad8daa3dd4ba8b5d043e18b14f23f0ff9
Joshua Hardman <jhardman0830@gmail.com>
2017-02-03 21:01:07 -0700

    Checks if sharp image has loaded before starting animation

--- public/js/landingPage.js
+++ public/js/landingPage.js
@@ -2,13 +2,29 @@ $(document).ready(function(){
   window.setTimeout(function(){
     $('.name-plaque').addClass('visible');
   },700);
+});
 
-  window.setTimeout(function(){
-    $('.js-background').addClass('hidden');
-    $('.item').addClass('moveRight');
-  },3000);
+var performAnimation = function(){
+  $('.js-background').addClass('hidden');
+  $('.item').addClass('moveRight');
 
   window.setTimeout(function(){
     $('.button-container').addClass('visible');
-  },5500);
+  },2000);
+}
+
+var imgLoaded = false;
+$('#bg').on('load', function() {
+    imgLoaded = true;
 });
+
+window.setTimeout(function(){
+  if( imgLoaded ) {
+    performAnimation();
+  } else {
+    $('#bg').on('load', function() {
+      imgLoaded = true;
+      performAnimation();
+    });
+  }
+},3000);
