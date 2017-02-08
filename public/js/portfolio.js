7e42bf9f48953069eb14ac2b0f0469f54c4ef842
Joshua Hardman <jhardman0830@gmail.com>
2017-02-07 21:14:59 -0700

    Adding scroll listener to highlight correct sidebar item based on the current section in the view port

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -4,7 +4,30 @@ window.addEventListener("beforeunload", function () {
   document.body.classList.add("animate-out");
 });
 
+var setActiveSideBarSection = function() {
+  function elementScrolled(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+    var elemTop = $(elem).offset().top + $( elem ).height()/2;
+    return ((elemTop <= docViewBottom) && (elemTop >= docViewTop));
+  }
+
+  var sectionNames = ['engagements','bridals','weddings','portraits'];
+
+  $('.js-side-btn').removeClass('selected');
+
+  sectionNames.forEach(function(sectionName){
+    if( elementScrolled('#' + sectionName ) ){
+      $('.js-' + sectionName ).addClass('selected');
+    } else {
+      $('.js-' + sectionName ).removeClass('selected');
+    }
+  });
+}
+
 $(document).ready(function(){
+
+  // set up smooth scrolling for any anchor tags
   $("a").on('click', function(event) {
     if (this.hash !== "") {
       event.preventDefault();
@@ -16,4 +39,14 @@ $(document).ready(function(){
       });
     }
   });
+
+  // set default on page load
+  setActiveSideBarSection();
+
+  // set up side bar button section listener
+  $(window).scroll(function(){
+      setActiveSideBarSection();
+  });
+
+
 });
